43064347893bd04a368dec664c0d162a6cd21a82
Nitish Nashine <nitishnashine@gmail.com>
2025-03-27 14:01:10 +0530

    add Home page and new components: Button, DataTable, and QueryEditor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <h1>Atlan Sql queries frontend Project folder structure setup</h1>
+      <Home />
     </>
   );
 }
