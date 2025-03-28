e3dc55de8aabd1d47d0792bd32da284bef9f884f
Nitish Nashine <nitishnashine@gmail.com>
2025-03-28 19:51:07 +0530

    remove DataTable component, add QueryHistory and ResultTable components, and integrate SQL editor with CSV export functionality

---
+++ src/assets/image.js
@@ -0,0 +1,4 @@
+import history from "./images/history.svg";
+import run from "./images/run.svg";
+
+export { history, run };
--- src/components/QueryEditor.jsx
+++ src/components/QueryEditor.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { run } from "../assets/image";
+import CodeMirror from "@uiw/react-codemirror";
+import { sql } from "@codemirror/lang-sql";
+import { oneDark } from "@codemirror/theme-one-dark";
 
-const QueryEditor = () => {
-  return <div>QueryEditor</div>;
+const QueryEditor = ({ onExecuteQuery, currentQuery, onQueryChange }) => {
+  const handleExecute = () => {
+    onExecuteQuery(currentQuery);
+  };
+
+  return (
+    <div className="query-editor">
+      <div className="editor-header">
+        <h2>SQL Editor</h2>
+        <button className="execute-btn" onClick={handleExecute}>
+          <img src={run} alt="Run Query" width={16} height={16} />
+          Run Query
+        </button>
+      </div>
+      <div className="editor-container">
+        <CodeMirror
+          value={currentQuery}
+          height="200px"
+          theme={oneDark}
+          extensions={[sql()]}
+          onChange={onQueryChange}
+          className="code-mirror-wrapper"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default QueryEditor;
