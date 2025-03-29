f3276d600828c1297e44bdd78b27586bc1fd5fc9
Nitish Nashine <nitishnashine@gmail.com>
2025-03-29 10:35:28 +0530

    update run.svg with new design and dimensions; adjust image size in QueryEditor; set fixed width for QueryHistory component

--- src/components/QueryHistory.jsx
+++ src/components/QueryHistory.jsx
@@ -2,7 +2,7 @@ import { history } from "../assets/image";
 
 const QueryHistory = ({ queries, onSelectQuery, selectedQuery }) => {
   return (
-    <div className="query-history">
+    <div className="query-history" style={{ width: "300px" }}>
       <div className="history-header">
         <img src={history} alt="Run Query" width={16} height={16} />
         <h3>Available Queries</h3>
