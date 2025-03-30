a8f4ec451ac4cd1ff007616ec4bc83754d5db7b0
Nitish Nashine <nitishnashine@gmail.com>
2025-03-30 11:24:37 +0530

    Add PieChartComponent and integrate it into QueryHistory; enhance styles for buttons and layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import QueryEditor from "../components/QueryEditor";
 import ResultTable from "../components/ResultTable";
 import QueryHistory from "../components/QueryHistory";
 import queryResult from "../assets/json/queryResult.json";
+import productData from "../assets/json/product.json";
 
 const predefinedQueries = queryResult.products.queries.map((q) => q.query);
 
@@ -10,12 +11,12 @@ const Home = () => {
   const [queryResults, setQueryResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentQuery, setCurrentQuery] = useState(predefinedQueries[0]);
-  const [hasExecutedQuery, setHasExecutedQuery] = useState(false); 
+  const [hasExecutedQuery, setHasExecutedQuery] = useState(false);
 
   const handleExecuteQuery = (query) => {
     console.log("Executing query:", query);
     setLoading(true);
-    setHasExecutedQuery(true); 
+    setHasExecutedQuery(true);
 
     const result = queryResult.products.queries.find((q) => q.query === query);
 
@@ -25,6 +26,11 @@ const Home = () => {
     }, 500);
   };
 
+  const handleShowAllProducts = () => {
+    setQueryResults(productData);
+    setHasExecutedQuery(true);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -32,6 +38,8 @@ const Home = () => {
           queries={predefinedQueries}
           onSelectQuery={(query) => setCurrentQuery(query)}
           selectedQuery={currentQuery}
+          showPieChart={hasExecutedQuery}
+          onShowAllProducts={handleShowAllProducts}
         />
       </div>
       <div className="main-content">
